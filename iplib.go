8f747bcbe2a082cfaac9b4fcf92a240e2bb88845
Alexander Svensson <gurkslask@gmail.com>
2025-09-05 11:17:54 +0200

    Got it to a good working state

---
+++ iplib.go
@@ -0,0 +1,232 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Octet struct {
+	num int
+	s   [8]bool
+}
+
+func (o *Octet) Setnum(n int) error {
+	// Set Octet with a int between 0-255
+	// Returns error if not between those numbers
+	o.num = n
+	err := o.Validate()
+	if err != nil {
+		return err
+	}
+
+	o.s = o.GetboolSlice()
+	return nil
+}
+
+func (o Octet) Printint() {
+	// Prints the decimal number of the octet
+	fmt.Println(o.num)
+}
+func (o Octet) GetString() string {
+	// Prints the decimal number as a string
+	return fmt.Sprint(o.num)
+}
+
+func (o Octet) Printbyte() {
+	// Prints the binary numbers as a string
+	fmt.Printf("%08b\n", byte(o.num))
+}
+func (o Octet) Getbyte() string {
+	// Returns the binary numbers as a string
+	return fmt.Sprintf("%08b", byte(o.num))
+}
+func (o Octet) GetbyteAsByte() byte {
+	// Returns the binary numbers as a string
+	return byte(o.num)
+}
+
+func (o Octet) Getbitfrombyte(n int) bool {
+	// Gets the n bit from the byte
+	// 1, 2, 4, 16, 32, 64, 128
+	return byte(o.num)&byte(n) > 0
+}
+func (o *Octet) GetboolSlice() [8]bool {
+	// Returns a slice of all the booleans
+	var res [8]bool
+	j := 1
+	for i := 0; i < 7; i++ {
+		res[i] = o.Getbitfrombyte(j)
+		j = j * 2
+	}
+	return res
+}
+func (o Octet) Validate() error {
+	if o.num < 0 || o.num > 255 {
+		return fmt.Errorf("Invalid number, not between 0-255, number was: %v", o.num)
+	} else {
+		return nil
+	}
+}
+func octetValidator(s string) error {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if i > 255 || i < 0 {
+		err = fmt.Errorf("Invalid number %v", s)
+	}
+	return err
+}
+
+type IPv4 struct {
+	address string
+	octets  [4]Octet
+}
+
+func NewIPv4FromBinary(b [4]byte) (IPv4, error) {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%v.", int(b[0])))
+	sb.WriteString(fmt.Sprintf("%v.", int(b[1])))
+	sb.WriteString(fmt.Sprintf("%v.", int(b[2])))
+	sb.WriteString(fmt.Sprintf("%v", int(b[3])))
+	return NewIPv4(sb.String())
+}
+
+func NewIPv4(n string) (IPv4, error) {
+	var ip IPv4
+	err := ip.SetAddress(n)
+	if err != nil {
+		return ip, err
+	}
+	return ip, nil
+}
+func NewIPv4SubnetMask(n string) (IPv4, error) {
+	var ip IPv4
+	err := ip.SetAddress(n)
+	if err != nil {
+		return ip, err
+	}
+	err = ip.CheckIfValidSubnetmask()
+	if err != nil {
+		return ip, err
+	}
+	return ip, nil
+}
+
+func (ip *IPv4) SetAddress(in string) error {
+	//fmt.Println(in)
+	err := ip.CheckIfValidIPAddress(in)
+	if err != nil {
+		return err
+	}
+	splittedoctets := strings.Split(in, ".")
+	for i := 0; i < 4; i++ {
+		err := ip.octets[i].Setnum(StrToInt(splittedoctets[i]))
+		if err != nil {
+			return err
+		}
+	}
+	ip.address = in
+	return nil
+}
+func (ip IPv4) PrintDecimal() string {
+	return ip.address
+}
+func (ip IPv4) PrintBinary() string {
+	var sb strings.Builder
+	for i := 0; i < 4; i++ {
+		sb.WriteString(ip.octets[i].Getbyte())
+	}
+	return sb.String()
+}
+// Returns [4]byte of the ip adress in 4 bytes
+func (ip IPv4) Getbytes() [4]byte {
+	var sb [4]byte
+	for i := range 4 {
+		sb[i] = ip.octets[i].GetbyteAsByte()
+	}
+	return sb
+}
+// Returns [32]bool of all the bits
+func (ip IPv4) GetBits() [32]bool {
+	var res [32]bool
+	b := ip.Getbytes()
+	for octetnum, octetbyte := range b {
+		bb := GetBoolsFromByte(octetbyte)
+		for bitnum, bit := range bb {
+			res[bitnum + octetnum * 8] = bit
+		}
+	}
+	return res
+}
+func (ip IPv4) CheckIfValidIPAddress(in string) error {
+	splittedoctets := strings.Split(in, ".")
+	if len(splittedoctets) != 4 {
+		return fmt.Errorf("Not a valid IP %v", in)
+	}
+	for i := 0; i < 4; i++ {
+		err := ip.octets[i].Setnum(StrToInt(splittedoctets[i]))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (ip IPv4) CheckIfValidSubnetmask() error {
+	bs := ip.PrintBinary()
+	//fmt.Println("SUBNET BINARY", bs)
+	firstzero := false
+	for k, v := range bs {
+		if !firstzero && v == '1' {
+			// We havent seen a zero, and the actual rune is 1, do nothing
+			continue
+		} else if firstzero && v == '1' {
+			//fmt.Println("SUBNET IP ADRESS", ip.address)
+			return fmt.Errorf("Not a valid subnetmask %x %#U, KEY %v", v, v, k)
+		} else if !firstzero && v == '0' {
+			firstzero = true
+		}
+	}
+	return nil
+}
+
+// Calculates a netaddress based on a ip and subnetmask, returns IPv4 struct
+func CalcNetAddress(ip IPv4, subnetmask IPv4) IPv4 {
+	ipb := ip.Getbytes()
+	smb := subnetmask.Getbytes()
+	var bb [4]byte
+	for k, _ := range ipb {
+		bb[k] = ipb[k] & smb[k]
+	}
+	resIP, _ := NewIPv4FromBinary(bb)
+	return resIP
+}
+// Calculates a broadcastaddress based on a ip and subnetmask, returns IPv4 struct
+func CalcBroadcastAddress (ip IPv4, subnetmask IPv4) IPv4 {
+	cidr := CalcCIDR(subnetmask)
+	ipb := ip.GetBits()
+	smb := subnetmask.GetBits()
+	var res [32]bool
+	var bb [4]byte
+	for i := range 32 {
+		if i < cidr {
+			res[i] = ipb[i] && smb[i]
+		} else {
+			res[i] = ipb[i] || !smb[i]
+		}
+	}
+	for i := range 4 {
+		bb[i] = GetByteFromBools(res[i * 8 : i * 8 + 8])
+	}
+	resIP, _ := NewIPv4FromBinary(bb)
+	return resIP
+}
+// Calculates a CIDR address from a subnet mask. Example: 255.255.255.0 = 24
+func CalcCIDR(subnetmask IPv4) int {
+	res := 0
+	for _, v := range subnetmask.Getbytes() {
+		bb := GetBoolsFromByte(v)
+		for _, bit := range bb {
+			if bit { res = res + 1} else { return res}
+		}
+	}
+	return res
+}
---
+++ model.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type (
+	errMsg error
+)
+
+const (
+	hotPink  = lipgloss.Color("#FF06B7")
+	darkGray = lipgloss.Color("#767676")
+	badass = lipgloss.Color("#B4D455")
+	orangeish = lipgloss.Color("#FF9911")
+)
+
+var (
+	inputStyle    = lipgloss.NewStyle().Foreground(hotPink)
+	continueStyle = lipgloss.NewStyle().Foreground(darkGray)
+	oct1Style     = lipgloss.NewStyle().Foreground(darkGray)
+	oct2Style     = lipgloss.NewStyle().Foreground(hotPink)
+	oct3Style     = lipgloss.NewStyle().Foreground(badass)
+	oct4Style     = lipgloss.NewStyle().Foreground(orangeish)
+)
+
+type model struct {
+	inputs       []textinput.Model // ipv4 Octets
+	focused      int               // which item our cursor is pointing at
+	err          error             // error?
+	ipaddr       IPv4              // the ip address
+	ipBinary     string
+	subnetmask   IPv4
+	subnetBinary string
+	netaddr	IPv4
+	broadcastaddr IPv4
+}
+
+func initialModel() model {
+	ip, _ := NewIPv4("1.2.3.4")
+	snm, _ := NewIPv4("255.255.255.0")
+	var inputs []textinput.Model = make([]textinput.Model, 8)
+	ipBinary := ip.PrintBinary()
+	subnetBinary := snm.PrintBinary()
+
+	inputs[0] = textinput.New()
+	inputs[0].Placeholder = "1"
+	inputs[0].SetValue("2")
+	inputs[0].Focus()
+	inputs[0].CharLimit = 3
+	inputs[0].Width = 4
+	inputs[0].Prompt = ""
+	inputs[0].Validate = octetValidator
+
+	inputs[1] = textinput.New()
+	inputs[1].Placeholder = "2"
+	inputs[1].SetValue("2")
+	inputs[1].CharLimit = 3
+	inputs[1].Width = 4
+	inputs[1].Prompt = ""
+	inputs[1].Validate = octetValidator
+
+	inputs[2] = textinput.New()
+	inputs[2].Placeholder = "3"
+	inputs[2].SetValue("2")
+	inputs[2].CharLimit = 3
+	inputs[2].Width = 4
+	inputs[2].Prompt = ""
+	inputs[2].Validate = octetValidator
+
+	inputs[3] = textinput.New()
+	inputs[3].Placeholder = "4"
+	inputs[3].SetValue("2")
+	inputs[3].CharLimit = 3
+	inputs[3].Width = 4
+	inputs[3].Prompt = ""
+	inputs[3].Validate = octetValidator
+
+	inputs[4] = textinput.New()
+	inputs[4].Placeholder = "4"
+	inputs[4].SetValue("255")
+	inputs[4].CharLimit = 3
+	inputs[4].Width = 4
+	inputs[4].Prompt = ""
+	inputs[4].Validate = octetValidator
+
+	inputs[5] = textinput.New()
+	inputs[5].Placeholder = "4"
+	inputs[5].SetValue("255")
+	inputs[5].CharLimit = 3
+	inputs[5].Width = 4
+	inputs[5].Prompt = ""
+	inputs[5].Validate = octetValidator
+
+	inputs[6] = textinput.New()
+	inputs[6].Placeholder = "4"
+	inputs[6].SetValue("255")
+	inputs[6].CharLimit = 3
+	inputs[6].Width = 4
+	inputs[6].Prompt = ""
+	inputs[6].Validate = octetValidator
+
+	inputs[7] = textinput.New()
+	inputs[7].Placeholder = "4"
+	inputs[7].SetValue("0")
+	inputs[7].CharLimit = 3
+	inputs[7].Width = 4
+	inputs[7].Prompt = ""
+	inputs[7].Validate = octetValidator
+	return model{
+		ipaddr:     ip,
+		subnetmask: snm,
+		inputs:     inputs,
+		focused:    0,
+		err:        nil,
+		ipBinary: ipBinary,
+		subnetBinary: subnetBinary,
+	}
+}
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			//if m.focused == len(m.inputs)-1 {
+			//return m, tea.Quit
+			//}
+			m.updateIPAddress()
+			m.updateIPSubnetmask()
+			m.netaddr = CalcNetAddress(m.ipaddr, m.subnetmask)
+			m.broadcastaddr = CalcBroadcastAddress(m.ipaddr, m.subnetmask)
+			m.ipBinary = m.ipaddr.PrintBinary()
+			m.subnetBinary = m.subnetmask.PrintBinary()
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyShiftTab, tea.KeyCtrlP:
+			m.prevInput()
+		case tea.KeyTab, tea.KeyCtrlN:
+			m.nextInput()
+		}
+		for i := range m.inputs {
+			m.inputs[i].Blur()
+		}
+		m.inputs[m.focused].Focus()
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+	return m, tea.Batch(cmds...)
+}
+func (m model) View() string {
+	return fmt.Sprintf(
+		` IP-Calculator
+
+IP-address
+ %s %s %s %s
+ %s %s %s %s
+
+ Binary
+ %s.%s.%s.%s
+
+ Subnetmask
+ %s %s %s %s
+ %s %s %s %s
+
+ Binary
+ %s.%s.%s.%s
+
+ netaddr
+ %s
+ broadcast
+ %s
+ %s
+ %s
+`,
+		inputStyle.Width(5).Render("Oct1"),
+		inputStyle.Width(5).Render("Oct2"),
+		inputStyle.Width(5).Render("Oct3"),
+		inputStyle.Width(5).Render("Oct4"),
+		m.inputs[0].View(),
+		m.inputs[1].View(),
+		m.inputs[2].View(),
+		m.inputs[3].View(),
+		oct1Style.Render(string(m.ipBinary[0:8])),
+		oct2Style.Render(string(m.ipBinary[8:16])),
+		oct3Style.Render(string(m.ipBinary[16:24])),
+		oct4Style.Render(string(m.ipBinary[24:32])),
+		inputStyle.Width(5).Render("Oct1"),
+		inputStyle.Width(5).Render("Oct2"),
+		inputStyle.Width(5).Render("Oct3"),
+		inputStyle.Width(5).Render("Oct4"),
+		m.inputs[4].View(),
+		m.inputs[5].View(),
+		m.inputs[6].View(),
+		m.inputs[7].View(),
+		oct1Style.Render(string(m.subnetBinary[0:8])),
+		oct2Style.Render(string(m.subnetBinary[8:16])),
+		oct3Style.Render(string(m.subnetBinary[16:24])),
+		oct4Style.Render(string(m.subnetBinary[24:32])),
+		m.netaddr.PrintDecimal(),
+		m.broadcastaddr.PrintDecimal(),
+		m.err,
+		continueStyle.Render("Continue ->"),
+	) + "\n"
+}
+
+// nextInput focuses the next input field
+func (m *model) nextInput() {
+	m.focused = (m.focused + 1) % len(m.inputs)
+}
+
+// prevInput focuses the previous input field
+func (m *model) prevInput() {
+	m.focused--
+	// Wrap around
+	if m.focused < 0 {
+		m.focused = len(m.inputs) - 1
+	}
+}
+func (m *model) updateIPAddress() {
+	ss := fmt.Sprintf("%v.%v.%v.%v", m.inputs[0].Value(), m.inputs[1].Value(), m.inputs[2].Value(), m.inputs[3].Value())
+	m.err = m.ipaddr.CheckIfValidIPAddress(ss)
+	if m.err != nil {
+		return
+	}
+	m.err = m.ipaddr.SetAddress(ss)
+}
+func (m *model) updateIPSubnetmask() {
+	ss := fmt.Sprintf("%v.%v.%v.%v", m.inputs[4].Value(), m.inputs[5].Value(), m.inputs[6].Value(), m.inputs[7].Value())
+	m.err = m.subnetmask.SetAddress(ss)
+	m.err = m.subnetmask.CheckIfValidSubnetmask()
+}
